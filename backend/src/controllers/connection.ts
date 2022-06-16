e7f7f24e64a61c78a4a1277d21d76bdd36deb442
Manoj <manoj@geekybones.com>
2022-06-16 12:18:50 +0530

    env added in frontend

--- backend/src/controllers/connection.ts
+++ backend/src/controllers/connection.ts
@@ -2,9 +2,73 @@ import { Controller, Param, Body, Get, Post, Put, Delete } from 'routing-control
 import ConnectionType from 'models/connectionType';
 import Connection from 'models/connection';
  
+// UPDATE 
+// DELETE
+// LOOKUP DATA
 @Controller('/api')
 export class ConnectionController {
  
+  @Get('/get-connection-type')
+  async getConnectionType() {
+    return await ConnectionType.aggregate([
+      {
+        '$project': {
+          'name': 1, 
+          'connectionTypeId': 1, 
+          'attributes': 1, 
+          '_id': 0
+        }
+      }
+    ]);
+   }
+
+   @Get('/get-connection/:id')
+   async getConnectionById(@Param('id') id: string) {
+     return await Connection.aggregate([
+       {
+          '$match': {
+            'connectionTypeId': id
+          }
+       },
+     ]);
+    }
+
+    @Post('/update-connection')
+    async updateConnectionById( @Body() body: any ) {
+      const { connectionTypeId } = body;
+      const updateItems = { ...body }
+      delete updateItems.connectionTypeId;
+      try {
+        await Connection.findOneAndUpdate({ connectionTypeId }, { ...updateItems })
+        return {
+          success: true,
+          message: "Connection updated successfully"
+        }
+      } catch (error) {
+        return {
+          success: false,
+          message: "Connection could not be updated. Please try after some time."
+        }
+      }
+    }
+
+    @Post('/delete-connection')
+    async deleteConnectionById( @Body() body: any ) {
+      const { connectionTypeId } = body;
+      try {
+        await Connection.findOneAndUpdate({ connectionTypeId })
+        return {
+          success: true,
+          message: "Connection deleted successfully"
+        }
+      } catch (error) {
+        return {
+          success: false,
+          message: "Connection could not be deleted. Please try after some time."
+        }
+      }
+    }
+
   @Post('/add-connection-type')
   async addConnectionType( @Body() body: any ) {
     const newConnectionType = new ConnectionType(body);
--- frontend/src/components/connection/AddConnection.js
+++ frontend/src/components/connection/AddConnection.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
 
 export const AddConnection = () => {
+
   return (
-    <div>addConnection</div>
+    <div>
+    <div>
+      <label>Name</label>
+      <input type={"text"} />
+    </div>
+    <div>
+      <label>Description</label>
+      <textarea type={"text"} />
+    </div>
+    <div>
+      <label>Type</label>
+      <select>
+        <option value="actual value 1">Display Text 1</option>
+        <option value="actual value 2">Display Text 2</option>
+        <option value="actual value 3">Display Text 3</option>
+      </select>
+    </div>
+    <div>
+      <label>User</label>
+      <input type={"text"} />
+      <label>Password</label>
+      <input type={"text"} />
+    </div>
+    <div>
+      <label>URL</label>
+      <input type={"text"} />
+    </div>
+    <button>Save</button>
+
+  </div>
   )
 }
   export default AddConnection;
