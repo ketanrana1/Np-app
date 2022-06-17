5f04c7ebe92ac9d18eb646fad1be57f8b610e721
Manoj <manoj@geekybones.com>
2022-06-17 10:42:38 +0530

    Firebase intergration working

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import firebaseConfig from "../firebase-config"
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+
+const DEFAULT_STATE = {
+  email: '',
+  password: ''
+}
 
 const Login = () => {
+
+  const [formState, setFormState] = useState(DEFAULT_STATE);
+
+  const loginButtonHandler = async () => {
+    const { email, password } = formState
+    try {
+      const response = await signInWithEmailAndPassword(getAuth(firebaseConfig),email, password); 
+      console.log("response", response.data)
+    } catch (error) {
+      console.log("error", error?.message)
+    }
+  }
+
+  const onInputChangeHandler = (e) => setFormState({...formState , [e.target.name]: e.target.value})
+
   return (
     <div className="login-form-cont">
-      <form>
         <h3 className="logo-text">Logo</h3>
       <div className="form-group">
-        <input type="email" className="form-control" placeholder="Usernam/Email"/>
+        <input type="email" name="email" className="form-control" placeholder="Email" required onChange={onInputChangeHandler}/>
       </div>
       <div className="form-group">
-        <input type="password" className="form-control" placeholder="Password" />
+        <input type="password" name="password" className="form-control" placeholder="Password" required onChange={onInputChangeHandler}/>
       </div>
       <div className="form-group frgt-pssw-cont">
         <a href="">Forgot Password</a>
@@ -19,9 +40,8 @@ const Login = () => {
         <label className="form-check-label" for="exampleCheck1">Remember</label>
       </div>
       <div className="submit-cont">
-         <input type="submit" value="Login" />
+         <input type="submit" value="Login" onClick={loginButtonHandler}/>
       </div>
-    </form>
     </div>
     )  
 }
